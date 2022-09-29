fccb1a4b14e021ba38f6c2dd4f02443353393525
Vlad <vladut_stoian@live.co.uk>
2022-09-29 11:25:08 +0100

    changes to env

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,5 +1,25 @@
 require("dotenv").config();
 const { Sequelize } = require("sequelize");
 
-exports.sequelize = new Sequelize(process.env.DATABASE_URL)
+// exports.sequelize = new Sequelize(process.env.DATABASE_URL)
 
+let sequelize;
+
+if (process.env.NODE_ENV === "prod")
+{
+
+    module.exports.sequelize = new Sequelize(`${process.env.DATABASE_URL} ? sslmode = require`, {
+        url: process.env.DATABASE_URI,
+        dialect: 'postgres',
+        dialectOptions: {
+            ssl: {
+                rejectUnauthorized: false,
+            }
+        }
+    });
+
+} else
+{
+
+    module.exports.sequelize = new Sequelize(process.env.DATABASE_URL);
+}
